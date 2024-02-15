d1dfce81e54893e56e7e57c1861ccccd935feeec
burhanfauzisalam <burhanfauzisalam@gmail.com>
2024-02-15 10:49:18 +0700

    add local time

--- models/thread.js
+++ models/thread.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const threadID = new mongoose.Schema({
   threadID: {
     type: String,
+    unique: true,
+  },
+  dateCreated: {
+    type: Date,
   },
 });
 
